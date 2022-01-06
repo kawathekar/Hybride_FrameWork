12c53be0eba0b28abe2fcebe3e097baba7bde774
shirisha28 <49709939+shirisha28@users.noreply.github.com>
2022-01-06 19:21:16 +0530

    DhruvCommit

---
+++ src/test/java/Test_Cases/Welcome.java
@@ -0,0 +1,11 @@
+package Test_Cases;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello and welcome");
+
+	}
+
+}
